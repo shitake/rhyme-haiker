0c4952285570cf643554942fc709384f51ee2e0f
shitake <p230584300813995212@gmail.com>
2016-06-21 21:01:11 +0900

    俳句を詠むクラスを一通り作成

--- poetry/haiker/haiker.py
+++ poetry/haiker/haiker.py
@@ -15,38 +15,68 @@ logger.addHandler(handler)
 
 class Haiker:
 
+    FIVE = 5
+    SEVEN = 7
+
     def __init__(self, words, chains):
         self.words = words
         self.chains = chains
 
-        self.word = ""
         self.word_list = []
+
+        self.word = ""
         self.text = ""
         self.text_list = []
         self.nodes = {}
         self.current_depth = 0
 
+        self.last_word_of_first_five = ""
+        self.last_word_of_seven = ""
+
+        self.first_five = ""
+        self.seven = ""
+        self.last_five = ""
+        self.haiku = ""
+
+    def compose(self):
+        """
+        俳句を詠む．
+        """
+        self.construct_first_five()
+        self.construct_seven()
+        self.construct_last_five()
+
+        logger.debug("[Haiku] >>> " + self.haiku)
+
+        return self.haiku
+
+    def property_initializer(func):
+        """
+        5, 7, 5 の各句を詠む前の初期化処理を行うためのデコレータ．
+        """
+
+        def wrapper(self):
+            self.word = ""
+            self.text = ""
+            self.text_list = []
+            self.nodes = {}
+            self.current_depth = 0
+            logger.debug("[Initialized]")
+
+            ret = func(self)
+
+            self.haiku = self.first_five + self.seven + self.last_five
+
+            return ret
+
+        return wrapper
+
+    @property_initializer
     def construct_first_five(self):
         """
         最初の5字を作成する．
         """
-        # first_five_list = []
-        # key_list = list((self.data).keys())
-
-        # 5字作成ループ
-        # for word in random.choice(key_list):
-        #     first_five_list.append(word)
-
-        #     first_five = ''.join(first_five_list)
-        #     if self._is_five_char(first_five):
-        #         return first_five_list
-
-        #     # key に続く val を取得
-
-        # return None
-
-        # 単語リスト取得
-        self.word_list = self._get_word_list()
+        self._get_word_list()
         # これまでの単語リスト
         self.nodes[self.current_depth] = copy.copy(self.word_list)
 
@@ -54,7 +84,7 @@ class Haiker:
             if self._is_empty_first_node():
                 return None  # TODO: 作成失敗時にエラーを返すようにする
 
-            logger.debug("-----------start: " + self.text)
+            logger.debug("-----------start 5: " + self.text)
             logger.debug("----- outer_word_list: " + repr(self.word_list))
 
             while True:
@@ -63,34 +93,43 @@ class Haiker:
                 logger.debug("----- inner_word_list: " + repr(self.word_list))
 
                 if not self.word:
-                    self.word_list = self._back_prev_word_list()
-                    logger.debug("======================== back: " + repr(self.word_list))
-                elif self._is_five_char(self.text + self.word):
-                    return self._return_completed_text()
-                elif len(self.text + self.word) < 5:
-                    # 5文字未満なら次の単語を取得する
-                    self.text = self.text + self.word
-                    self.text_list.append(self.word)
+                    self._back_prev_word_list()
+                    logger.debug(
+                        "======================== back: " +
+                        repr(self.word_list)
+                    )
+                elif self._is_n_char(Haiker.FIVE, self.text + self.word):
+                    self.last_word_of_first_five = self.word
+                    self.first_five = self._create_completed_text()
+                    return self.first_five
+                elif self._is_less_than_n_char(Haiker.FIVE, self.text + self.word):
+                    self._get_next_word()
                     break
                 else:
                     # 5文字より長くなったら別の単語を取得しなおす
                     logger.debug(
                         "----- else" +
-                        self.text +
-                        self.word +
+                        repr(self.text) +
+                        repr(self.word) +
                         repr(self.word_list) +
-                        self.current_depth +
+                        repr(self.current_depth) +
                         repr(self.nodes)
                     )
                     pass
 
             # 単語をkeyに単語リスト取得
             self._get_next_word_list()
-            self.word_list = self.nodes[self.current_depth]
 
     def _get_word_list(self):
         """単語リストの取得"""
-        return list((self.chains).keys())
+        self.word_list = list((self.chains).keys())
+
+    def _is_empty_first_node(self):
+        """最初のノードが空の場合真"""
+        if self.nodes[0] == []:
+            return True
+        else:
+            return False
 
     def _get_word(self):
         """単語をランダムに1つ取得"""
@@ -103,32 +142,41 @@ class Haiker:
     def _back_prev_word_list(self):
         """単語リストが空になったら，前の単語リストに戻る"""
         self.current_depth -= 1
-        return self.nodes[self.current_depth]
+        self.word_list = self.nodes[self.current_depth]
 
-    def _return_completed_text(self):
+    def _create_completed_text(self):
         """完成形の文章を返す"""
         logger.debug("[Completed]: " + self.text + self.word)
         return self.text + self.word
 
-    def _is_n_char(self, n, pronunciation):
+    def _is_n_char(self, n, text):
         """
-        与えられた文章の発音が n 文字であれば真．
+        与えられた文章(発音)が n 文字であれば真．
 
         Args:
           n: 文字数．int．
-          pronunciation: 発音．
+          text: 文章(発音)．
         """
-        if len(pronunciation) == n:
+        if len(text) == n:
             return True
         else:
             return False
 
-    def _is_five_char(self, word):
-        """文字数チェック"""
-        if len(word) == 5:
+    def _is_less_than_n_char(self, n, text):
+        """
+        与えられた文章が，n文字未満であれば真
+        """
+        if len(text) < n:
             return True
         else:
             return False
+
+    def _get_next_word(self):
+        """
+        次の単語を取得する．
+        """
+        self.text = self.text + self.word
+        self.text_list.append(self.word)
 
     def _get_next_word_list(self):
         """単語を key に単語リストを取得"""
@@ -137,29 +185,138 @@ class Haiker:
         # data に key が存在しない場合，
         # ランダムに取得する
         if self.word not in keys:
+            if not self.word:
+                self.word = "[Empty]"
             logger.debug("-----: " + self.word + "not in")
             self.word = random.choice(list(keys))
-            logger.debug("----- new word:" + self.word)
+            logger.debug("----- new word: " + self.word)
         self.nodes[self.current_depth] = self.chains[self.word]
+        self.word_list = self.nodes[self.current_depth]
 
-    def _is_empty_first_node(self):
-        """最初のノードが空の場合真"""
-        if self.nodes[0] == []:
-            return True
-        else:
-            return False
-
-    def construct_seven(self, last_word):
+    @property_initializer
+    def construct_seven(self):
         """
         真ん中の7字を作成する．
-
-        Args:
-          last_word: 最初の5字における最後の単語
         """
-        # return seven_list
+        if not self.last_word_of_first_five:
+            raise ConstructionError(
+                self.last_word_of_first_five,
+                self.last_word_of_seven
+            )
+        logger.debug(
+            "----- last_word_of_first_five: " + self.last_word_of_first_five
+        )
 
-    def construct_last_five(self, last_word):
+        self.word = self.last_word_of_first_five
+        self._get_word_list()
+        # これまでの単語リスト初期化
+        self.nodes[self.current_depth] = copy.copy(self.word_list)
+
+        while True:
+            # 単語をkeyに単語リスト取得
+            self._get_next_word_list()
+
+            if self._is_empty_first_node():
+                return None  # TODO: 作成失敗時にエラーを返すようにする
+
+            logger.debug("-----------start 7: " + self.text)
+            logger.debug("----- outer_word_list: " + repr(self.word_list))
+
+            while True:
+                # 単語1つ取得
+                self._get_word()
+                logger.debug("----- inner_word_list: " + repr(self.word_list))
+
+                if not self.word:
+                    self._back_prev_word_list()
+                    logger.debug(
+                        "======================== back: " +
+                        repr(self.word_list)
+                    )
+                elif self._is_n_char(Haiker.SEVEN, self.text + self.word):
+                    self.last_word_of_seven = self.word
+                    self.seven = self._create_completed_text()
+                    return self.seven
+                elif self._is_less_than_n_char(Haiker.SEVEN, self.text + self.word):
+                    self._get_next_word()
+                    break
+                else:
+                    # 5文字より長くなったら別の単語を取得しなおす
+                    logger.debug(
+                        "----- else: " +
+                        repr(self.text) +
+                        repr(self.word) +
+                        repr(self.word_list) +
+                        repr(self.current_depth) +
+                        repr(self.nodes)
+                    )
+                    pass
+
+    @property_initializer
+    def construct_last_five(self):
         """
         最後の5字を作成する．
         """
-        # return last_five_list
+        if not self.last_word_of_seven:
+            raise ConstructionError(
+                self.last_word_of_first_five,
+                self.last_word_of_seven
+            )
+        logger.debug(
+            "----- last_word_of_seven: " + self.last_word_of_seven
+        )
+
+        self.word = self.last_word_of_seven
+        self._get_word_list()
+        # これまでの単語リスト初期化
+        self.nodes[self.current_depth] = copy.copy(self.word_list)
+
+        while True:
+            # 単語をkeyに単語リスト取得
+            self._get_next_word_list()
+
+            if self._is_empty_first_node():
+                return None  # TODO: 作成失敗時にエラーを返すようにする
+
+            logger.debug("-----------start 7: " + self.text)
+            logger.debug("----- outer_word_list: " + repr(self.word_list))
+
+            while True:
+                # 単語1つ取得
+                self._get_word()
+                logger.debug("----- inner_word_list: " + repr(self.word_list))
+
+                if not self.word:
+                    self._back_prev_word_list()
+                    logger.debug(
+                        "======================== back: " +
+                        repr(self.word_list)
+                    )
+                elif self._is_n_char(Haiker.FIVE, self.text + self.word):
+                    self.last_word_of_last_five = self.word
+                    self.last_five = self._create_completed_text()
+                    return self.last_five
+                elif self._is_less_than_n_char(Haiker.FIVE, self.text + self.word):
+                    self._get_next_word()
+                    break
+                else:
+                    # 5文字より長くなったら別の単語を取得しなおす
+                    logger.debug(
+                        "----- else: " +
+                        repr(self.text) +
+                        repr(self.word) +
+                        repr(self.word_list) +
+                        repr(self.current_depth) +
+                        repr(self.nodes)
+                    )
+                    pass
+
+
+class ConstructionError(Exception):
+
+    def __init__(self, last_five, last_seven):
+        self.last_five = last_five
+        self.last_seven = last_seven
+
+    def __str__(self):
+        return repr(self.last_five, self.last_seven)
--- tests/test_haiker.py
+++ tests/test_haiker.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from poetry.haiker.haiker import Haiker
+from poetry.haiker.haiker import ConstructionError
 import unittest
 
 
@@ -16,6 +17,10 @@ class TestHaiker(unittest.TestCase):
                 "い": ["古池"],
                 "う": ["蛙"]
             }
+        )
+        self.assertIsInstance(
+            haiker.construct_first_five(),
+            str
         )
         self.assertEqual(
             len(haiker.construct_first_five()),
@@ -53,5 +58,114 @@ class TestHaiker(unittest.TestCase):
             haiker.construct_first_five(),
             None
         )
+
+    def test_is_n_char(self):
+        haiker = Haiker("", {})
+
+        self.assertTrue(
+            haiker._is_n_char(5, "aiueo")
+        )
+        self.assertTrue(
+            haiker._is_n_char(5, "フルイケヤ")
+        )
+        self.assertTrue(
+            haiker._is_n_char(7, "カワズトビコム")
+        )
+
+        self.assertFalse(
+            haiker._is_n_char(5, "カワズトビコム")
+        )
+
+    def test_is_less_than_n_char(self):
+        haiker = Haiker("", {})
+
+        self.assertTrue(
+            haiker._is_less_than_n_char(5, "aiue")
+        )
+        self.assertTrue(
+            haiker._is_less_than_n_char(5, "フルイケ")
+        )
+        self.assertTrue(
+            haiker._is_less_than_n_char(7, "カワズトビコ")
+        )
+
+        self.assertFalse(
+            haiker._is_less_than_n_char(5, "フルイケヤ")
+        )
+
+    def test_construct_seven(self):
+        haiker = Haiker(
+            "",
+            {
+                "古池": ["あ", "い", "う"],
+                "蛙": ["あ", "い"],
+                "あ": ["古池", "蛙"],
+                "い": ["古池"],
+                "う": ["蛙"]
+            }
+        )
+        haiker.construct_first_five()
+        self.assertEqual(
+            len(haiker.construct_seven()),
+            7
+        )
+
+        # 最初の5字の最後の単語が登録されていない場合，例外を返すことを確認
+        haiker = Haiker(
+            "",
+            {}
+        )
+        haiker.construct_first_five()
+        with self.assertRaises(ConstructionError):
+            haiker.construct_seven()
+
+    def test_construct_last_five(self):
+        haiker = Haiker(
+            "",
+            {
+                "古池": ["あ", "い", "う"],
+                "蛙": ["あ", "い"],
+                "あ": ["古池", "蛙"],
+                "い": ["古池"],
+                "う": ["蛙"]
+            }
+        )
+        haiker.construct_first_five()
+        haiker.construct_seven()
+        self.assertEqual(
+            len(haiker.construct_last_five()),
+            5
+        )
+
+        # 7字の最後の単語が登録されていない場合，例外を返すことを確認
+        haiker = Haiker(
+            "",
+            {
+                "あ": ["い"],
+                "い": ["あ"]
+            }
+        )
+        haiker.construct_first_five()
+        haiker.construct_seven()
+        haiker.last_word_of_seven = ""
+        with self.assertRaises(ConstructionError):
+            haiker.construct_last_five()
+
+    def test_compose(self):
+        haiker = Haiker(
+            "",
+            {
+                "古池": ["あ", "い", "う"],
+                "蛙": ["あ", "い"],
+                "あ": ["古池", "蛙"],
+                "い": ["古池"],
+                "う": ["蛙"]
+            }
+        )
+        self.assertTrue(
+            haiker.compose()
+        )
+
+
 if __name__ == '__main__':
     unittest.main()
