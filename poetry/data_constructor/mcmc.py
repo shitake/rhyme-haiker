3f457b8ffa7898580d0c61be478e717226e9ddcb
shitake <p230584300813995212@gmail.com>
2016-06-21 09:34:40 +0900

    mcmc修正

--- poetry/data_constructor/mcmc.py
+++ poetry/data_constructor/mcmc.py
@@ -9,49 +9,23 @@ logger.addHandler(logging.StreamHandler())
 
 class MCMC:
 
-    def __init__(self, read_data):
-        self.read_data = read_data  # TODO: TextReader からデータを取得するように変更
+    def __init__(self):
+        # self.read_data = read_data  # TODO: TextReader からデータを取得するように変更
+        self.read_data = ""
 
-    def create_chains_list(self):
+    def create_chains_list(self, read_data):
         """
-        入力: 分かち書き後のリスト
+        文章から単語の前後関係を抽出する．
+
+        Args:
+          read_data: 未加工の文章
         """
-        # 先頭の2つの単語からなるリストを
-        # construct_chain() へわたす
-        chain = self._construct_chain()
-        self._insert_chained_words(chain)
+        self.read_data = read_data
+        text_list = self._wakatu()
+        chain = self._create_chain_dict(text_list)
+        # self._insert_chained_words(chain)
 
-        return {
-            "word": csv_data[self.WORD],
-            "part": csv_data[self.PART],
-            "vowel": self.vowel_pronounciation,
-            "length": len(self.vowel_pronounciation)
-        }
-
-    def _construct_chain_dict(self):
-        """
-        単語の前後関係を持つ dict を作成．
-        """
-        text = []
-        for line in self._wakatu():  # TODO: 再考
-            for word in line:
-                text.append(word)
-        text_set = list(set(text))
-
-        # 句読点を key としないようにする
-        # TODO: 他の記号とかも考慮すべき
-        excluded_words = '、。，．'
-
-        chain = {}
-        for i in range(len(text_set)):
-            candidates = []
-            current_key = text_set[i]
-            for j in range(len(text) - 1):
-                if current_key == text[j] and text[j][-1] not in excluded_words:
-                    candidates.append(str(text[j + 1]))
-            chain[current_key] = candidates
-
-        return chain
+        return chain  # TODO: words データ作成側と同じような出力型にする
 
     def _wakatu(self):
         """
@@ -59,10 +33,65 @@ class MCMC:
         """
         # TODO: ジェネレータ化
         m = MeCab.Tagger('-Owakati')
-        return m.parse(self.read_data).split("\n")
+        return m.parse(self.read_data).split()
 
-    def insert_chained_words(self, word_dict):
+    def _create_chain_dict(self, text_list):
         """
-        2つの単語からなる dict を
-        DB へ登録する．
+        単語の前後関係を持つ dict を作成．
+
+        Args:
+          text_list: 分かち書き後のリスト
         """
+        unique_text_list = list(set(text_list))
+
+        chain = {}
+        for current_key in unique_text_list:
+            chain[current_key] = self._create_next_word_list(
+                current_key,
+                text_list
+            )
+
+        return chain
+
+    def _create_next_word_list(self, current_word, text_list):
+        """
+        指定した単語に続く単語のリストを
+        文章中から作成する．
+
+        Args:
+          current_word: 指定した単語
+          text_list: 文章を分かち書きした後のリスト
+        """
+        # 句読点を key としないようにする
+        # TODO: 他の記号とかも考慮すべき
+        # TODO: is で比較すべき？
+        excluded_words = '、。，．'
+
+        next_word_list = []
+
+        for i in range(len(text_list) - 1):
+            last_char = text_list[i][-1]
+
+            if text_list[i] == current_word and \
+                    last_char not in excluded_words:
+
+                next_word = str(text_list[i + 1])
+                next_word_list.append(next_word)
+
+        return next_word_list
+
+    # def _create_text_set(self):
+    #     """
+    #     単語の set を作成
+    #     """
+    #     text = []
+    #     for line in self._wakatu():  # TODO: 再考
+    #         for word in line:
+    #             text.append(word)
+    #     return list(set(text))
+
+    # def insert_chained_words(self, word_dict):
+    #     """
+    #     2つの単語からなる dict を
+    #     DB へ登録する．
+    #     """
