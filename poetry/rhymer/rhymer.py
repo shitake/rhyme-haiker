891b946c651d10b3b6b529d1cbd2a5afb969a9bd
shitake <p230584300813995212@gmail.com>
2016-07-05 22:32:44 +0900

    韻を踏む処理を追加

---
+++ poetry/rhymer/rhymer.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+from logging import getLogger
+from logging import StreamHandler
+from logging import DEBUG
+
+logger = getLogger(__name__)
+handler = StreamHandler()
+handler.setLevel(DEBUG)
+logger.setLevel(DEBUG)
+logger.addHandler(handler)
+
+
+class Rhymer:
+
+    def __init__(self):
+        self.point = 0
+
+    def is_rhymed(self, first_pron, second_pron):
+        """
+        入力として読みを受け，
+        末尾1字以上が一致していれば韻を踏んでいるとする．
+
+        Args:
+          first_pron: 一つ目の読み
+          second_pron: 二つ目の読み
+        """
+        if first_pron[-1] == second_pron[-1]:
+            return True
+        else:
+            return False
+
+    def evaluate(self, first_pron, second_pron):
+        """
+        一致している字数をポイントとして評価する
+        """
+        self.point = 0
+
+        for fp, sp in zip(first_pron[::-1], second_pron[::-1]):
+            logger.debug("fp: " + fp + ", " + "sp: " + sp)
+            if fp == sp:
+                self.point += 1
+            else:
+                break
+        return self.point
---
+++ tests/test_rhymer.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+from poetry.rhymer.rhymer import Rhymer
+import unittest
+
+
+class TestRhymer(unittest.TestCase):
+
+    def test_is_rhymed(self):
+        rhymer = Rhymer()
+
+        first_pron = "アイウエオ"
+        second_pron = "アイウエオ"
+
+        self.assertTrue(
+            rhymer.is_rhymed(
+                first_pron,
+                second_pron
+            )
+        )
+
+    def test_evaluate(self):
+        rhymer = Rhymer()
+
+        first_pron = "アイウエオ"
+        second_pron = "アイウエオ"
+
+        self.assertEqual(
+            rhymer.evaluate(
+                first_pron,
+                second_pron
+            ),
+            5
+        )
+
+        first_pron = "アイウエオ"
+        second_pron = "カキクケオ"
+
+        self.assertEqual(
+            rhymer.evaluate(
+                first_pron,
+                second_pron
+            ),
+            1
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
