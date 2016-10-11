af3e850b63c8091cc6c6d6c2e9be02e47e53d330
shitake <p230584300813995212@gmail.com>
2016-10-11 13:15:41 +0900

    README 整理

---
+++ README.rst
@@ -0,0 +1,36 @@
+=================
+Rhyme Haiker
+=================
+
+押韻俳句生成器
+
+A rhyming haiku generator.
+
+
+=================
+Install
+=================
+
+You can install rhyme-haiker using:
+
+::
+
+    $ pip install rhyme-haiker
+
+
+=================
+Usage
+=================
+
+1. Prepare a Japanese text corpus.
+2. Preprocessing.
+
+   ::
+
+       $ haiku prep hoge_corpus.txt
+
+3. Compose.
+
+   ::
+
+       $ haiku compose
--- setup.py
+++ setup.py
@@ -1,17 +1,23 @@
 from setuptools import setup
 
-with open('README.txt') as f:
+with open('README.rst') as f:
     readme = f.read()
 
 setup(
     name="RhymeHaiker",
-    version="1.1.0",
+    version="2.0.0",
     description='A rhyming haiku generator.',
     long_description=readme,
     url='',
     author="pochiyuru",
     license='MIT',
-    classifiers=[],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: End Users/Desktop',
+        'Natural Language :: Japanese',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Communications'
+    ],
     keywords='rhyme haiku',
     packages=[
         'poetry',
